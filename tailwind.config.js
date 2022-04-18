2b9d1a3f6085210be29c4ae2727ccb984459ab34
hossein <hossein13mousavi@gmail.com>
2022-04-18 10:36:15 +0430

    fix(tailwind)!: fix tailwind configuration based on project folder hierarchy changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   prefix: '',
   important: true,
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
