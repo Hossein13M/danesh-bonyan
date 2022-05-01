4ce67092ad912f83503d394a42e168a1b9b13b19
hossein <hossein13mousavi@gmail.com>
2022-05-01 15:01:22 +0430

    feat(layout): implement footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="flex justify-center bg-slate-200 dark:bg-custom-slate-100 px-2 md:px-20 items-center w-full py-4">
+        <a className="flex items-center" href="https://hmousavi.dev" target="blank">
+          <h1 className="text-black sm:text-2xl ml-2 dark:text-white">footer</h1>
+        </a>
+      </div>
+    </>
+  );
+}
