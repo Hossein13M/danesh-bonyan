48165c2aed0e8df70e7ddc0f321e449318695a25
hossein <hossein13mousavi@gmail.com>
2022-05-08 14:13:37 +0430

    style(layout): change layout background color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Footer from './footer';
 
 export default function Layout({ children }: any) {
   return (
-    <div className="flex flex-col min-h-screen items-stretch bg-stone-100 dark:bg-stone-800">
+    <div className="flex flex-col min-h-screen items-stretch bg-slate-50 dark:bg-stone-800">
       <Header />
       <div className="flex-grow text-black dark:text-zinc-50 ">
         <div className="flex justify-center pt-5">{children}</div>
