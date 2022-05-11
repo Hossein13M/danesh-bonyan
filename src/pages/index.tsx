d898ac6166614e2da8d4e7e146682eef2edb1b26
hossein <hossein13mousavi@gmail.com>
2022-05-11 16:14:06 +0430

    feat(index): add table and input component in the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import GeneralTable from '../components/general-table';
+import { GeneralInput } from '../components/general-input';
 
 const Home: NextPage = () => {
   return (
@@ -11,41 +13,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+      <main className="flex flex-col justify-center items-center mt-4 static overflow-hidden">
+        <h1>جستجوی آگهی شغلی</h1>
+        <div className="flex items-center justify-center w-full" style={{ height: '100px' }}>
+          <GeneralInput />
+        </div>
+        <div className="">
+          <GeneralTable />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <h1>footer</h1>
-      </footer>
     </div>
   );
 };
