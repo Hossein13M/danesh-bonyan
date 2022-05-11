4b6881893dddbbe872406a71941e39373ae9184e
hossein <hossein13mousavi@gmail.com>
2022-05-11 16:12:29 +0430

    feat(layout): enhance layout semantic html and UI

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,15 +4,17 @@ import SideBar from './sidebar';
 
 export default function Layout({ children }: any) {
   return (
-    <div className="flex flex-col min-h-screen items-stretch bg-slate-50 dark:bg-stone-800">
-      <SideBar />
-      <Header />
-      <div className="flex-grow text-black dark:text-zinc-50">
+    <div className="flex flex-col  items-stretch min-h-screen bg-slate-50 dark:bg-stone-800" style={{ height: '50px' }}>
+      <header className="flex-shrink-0">
+        <SideBar />
+        <Header />
+      </header>
+      <main className="flex-grow text-black dark:text-zinc-50 py-4 overflow-y-scroll" style={{ height: 'calc(100vh - 200px)' }}>
         <div className="flex justify-center pt-5">{children}</div>
-      </div>
-      <div className="flex-shrink-0">
+      </main>
+      <footer className="flex-shrink-0">
         <Footer />
-      </div>
+      </footer>
     </div>
   );
 }
