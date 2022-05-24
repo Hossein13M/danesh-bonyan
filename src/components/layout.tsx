b887b437e4f8b8e7912ca87a03696656cae13787
hossein <hossein13mousavi@gmail.com>
2022-05-24 13:55:11 +0430

    style(layout): remove unnecessary padding from the layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,12 +4,12 @@ import SideBar from './sidebar';
 
 export default function Layout({ children }: any) {
   return (
-    <div className="flex flex-col  items-stretch min-h-screen bg-slate-50 dark:bg-stone-800" style={{ height: '50px' }}>
+    <div className="flex flex-col items-stretch min-h-screen bg-slate-50 dark:bg-stone-800" style={{ height: '50px' }}>
       <header className="flex-shrink-0">
         <SideBar />
         <Header />
       </header>
-      <main className="main-content-layout flex-grow text-black dark:text-zinc-50 py-4 overflow-y-scroll" style={{ height: 'calc(100vh - 200px)' }}>
+      <main className="main-content-layout flex-grow text-black dark:text-zinc-50 overflow-y-scroll" style={{ height: 'calc(100vh - 200px)' }}>
         <div className="flex justify-center pt-5">{children}</div>
       </main>
       <footer className="flex-shrink-0">
