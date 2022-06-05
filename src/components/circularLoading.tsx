ac5d4579d648f9129467f16a439dfa4aec0ada2b
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-06-05 14:27:22 +0430

    style(circularLoading): change loading spinner thickness

--- src/components/circularLoading.tsx
+++ src/components/circularLoading.tsx
@@ -5,7 +5,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 export default function CircularLoading() {
   return (
     <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
-      <CircularProgress color="primary" size={80} thickness={8} />
+      <CircularProgress color="primary" size={80} thickness={5} />
     </Stack>
   );
 }
