54d402539c54aa8d3609b2f5db7e27c1975855b3
hossein <hossein13mousavi@gmail.com>
2022-06-15 08:22:50 +0430

    style(global): change dark theme layout color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import SideBar from './sidebar';
 
 export default function Layout({ children }: any) {
   return (
-    <div className="flex flex-col items-stretch min-h-screen bg-slate-50 dark:bg-stone-800" style={{ height: '50px' }}>
+    <div className="flex flex-col items-stretch min-h-screen bg-slate-50 dark:bg-slate-900" style={{ height: '50px' }}>
       <header className="flex-shrink-0">
         <SideBar />
         <Header />
