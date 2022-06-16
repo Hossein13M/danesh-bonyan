824ae7fd6b788aefbcb62d3296d1d88be9328d8d
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-16 11:45:13 +0430

    chore(emptyState): temp commit to check iamge url

--- src/components/emptyState.tsx
+++ src/components/emptyState.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
+import Image from 'next/image';
 
 export default function EmptyState() {
-  return <h1>Empty State</h1>;
+  return <Image src="/assets/preview.png" alt="dastan" width={800} height={418} className="gray-filter hover:no-filter" />;
 }
